7de7ba687300e3ba47cda7a12bdf4de17b6474a8
Hermitwolf123 <162417178+Hermitwolf123@users.noreply.github.com>
2024-03-07 07:43:32 +0000

    added ex01a

---
+++ ex01a_comments_in_python.py
@@ -0,0 +1,25 @@
+print('Below line of code is one liner comment: ')
+
+# this is single line comment..
+
+
+
+print('Below line of code is multi liner comment: ')
+
+
+'''
+this is multi  line comment..
+
+this is multi line comment..
+
+this is multi line comment..
+
+'''
+
+"""
+this is multi  line comment..
+
+this is multi line comment..
+
+this is multi line comment..
+"""
